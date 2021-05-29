b37233a1b75de6600bcc4b2c44be04f5ab986995
Ahmed Eid <ahmedeid.webdev@gmail.com>
2021-05-29 18:24:14 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+       Hello world 
+      </div>
+    )
+  }
+}
+
+export default App
+
